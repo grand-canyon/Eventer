f45922b9eab2dc127ae3b56a5efb73ee0a4942e5
unbelt <unbelt@outlook.com>
2015-06-03 20:28:19 +0300

    Code & Style reorganizing; Style fixes

---
+++ Eventer/Eventer.Web/Infrastructure/Common/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventer.Web.Infrastructure.Common
+{
+    public static class Config
+    {
+        public const string AppName = "eventer";
+        public static string SiteDescription = "Simple Event System";
+        public const string TeamName = "Grand Canyon";
+        public const string DefaultAvatar = "~/Content/Images/avatar.png";
+    }
+}
